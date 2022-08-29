f120364e3a3d39f7e2d12636ecb2636ea16d3e17
eduardoslg <eduardo.seve@hotmail.com>
2022-08-29 07:29:17 -0300

    atualização página teste

--- src/pages/Teste/index.js
+++ src/pages/Teste/index.js
@@ -1,20 +1,245 @@
 
 import './teste.css';
-import Title from '../../components/Title';
+import { useState, useEffect } from 'react';
+
 import Header from '../../components/Header';
+import Title from '../../components/Title';
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+
 import firebase from '../../services/firebaseConnection';
-import { FiAperture } from 'react-icons/fi';
+import Modal from '../../components/Modal';
+
 
 export default function Teste(){
-  return (
+
+
+  const [loadCustomers, setLoadCustomers] = useState(true);
+  const [customers, setCustomers] = useState([]);
+  const [customerSelected, setCustomerSelected] = useState(0);
+
+  const [chamados, setChamados] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [lastDocs, setLastDocs] = useState();
+
+  const [showPostModal, setShowPostModal] = useState(false);
+  const [detail, setDetail] = useState();
+
+ 
+  
+  useEffect(()=> {
+    async function loadCustomers(){
+      await firebase.firestore().collection('customers')
+      .get()
+      .then((snapshot)=>{
+        let lista = [];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            nomeFantasia: doc.data().nomeFantasia
+          })
+        })
+
+        if(lista.length === 0){
+          console.log('NENHUMA EMPRESA ENCONTRADA');
+          setCustomers([ { id: '1', nomeFantasia: 'FREELA' } ]);
+          setLoadCustomers(false);
+          return;
+        }
+
+        setCustomers(lista);
+        setLoadCustomers(false);
+
+
+      })
+      .catch((error)=>{
+        console.log('DEU ALGUM ERRO!', error);
+        setLoadCustomers(false);
+        setCustomers([ { id: '1', nomeFantasia: '' } ]);
+      })
+
+    }
+
+    loadCustomers();
+
+  }, []);
+
+  async function loadChamados(e){
+     let listRef = await firebase.firestore().collection("chamados")
+    .where("clientId", "==", customers[e.target.value].id)
+    .get()
+    .then((snapshot) => {
+      updateState(snapshot)
+      const isCollectionEmpty = snapshot.size === 0;
+
+      if(!isCollectionEmpty){
+        let lista = [];
+  
+        snapshot.forEach((doc)=>{
+          lista.push({
+            id: doc.id,
+            assunto: doc.data().assunto,
+            cliente: doc.data().cliente,
+            clienteId: doc.data().clienteId,
+            created: doc.data().created,
+            createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+            status: doc.data().status,
+            complemento: doc.data().complemento
+          })
+        })
+        
+    }})
+    .catch((err)=>{
+      console.log('Deu algum erro: ', err);
+      setLoadingMore(false);
+    })
+
+    setLoading(false);
+
+  }
+
+  async function updateState(snapshot){
+    const isCollectionEmpty = snapshot.size === 0;
+
+    if(!isCollectionEmpty){
+      let lista = [];
+
+      snapshot.forEach((doc)=>{
+        lista.push({
+          id: doc.id,
+          assunto: doc.data().assunto,
+          cliente: doc.data().cliente,
+          clienteId: doc.data().clienteId,
+          created: doc.data().created,
+          createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+          status: doc.data().status,
+          complemento: doc.data().complemento
+        })
+      })
+
+      const lastDoc = snapshot.docs[snapshot.docs.length -1]; //Pegando o ultimo documento buscado
+      
+      setChamados(chamados => [...chamados, ...lista]);
+      setLastDocs(lastDoc);
+
+    }else{
+      setIsEmpty(true);
+    }
+
+    setLoadingMore(false);
+
+  }
+
+
+
+  function togglePostModal(item){
+    setShowPostModal(!showPostModal) //trocando de true pra false
+    setDetail(item);
+  }
+  
+  //Chamado quando troca de cliente
+  function handleChangeCustomers(e){
+    //console.log('INDEX DO CLIENTE SELECIONADO: ', e.target.value);
+    console.log('Cliente selecionado ', customers[customerSelected].id)
+    console.log('Cliente selecionado ', customers[e.target.value])
+    console.log('Cliente selecionado ', customers[e.target.value].id)
+    setCustomerSelected(e.target.value);
+  }
+
+
+  return(
     <div>
       <Header/>
 
       <div className="content">
-        <Title name="Teste">
-          <FiAperture size={25} />
+        <Title name="Atendimentos">
+          <FiMessageSquare size={25} />
         </Title>
+
+        <label>Cliente</label>
+
+
+            <select value={customerSelected} onChange={handleChangeCustomers} >
+            {customers.map((item, index) => {
+              return(
+                <option key={item.id} value={index} >
+                  {item.nomeFantasia}
+                </option>
+              )
+            })}
+          </select>
+
+          <button className="action"  onClick={ loadChamados }>Buscar
+          </button>
+        
+
+        {chamados.length === 0 ? (
+          <div className="container dashboard">
+            <span>Nenhum chamado registrado...</span>
+
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+          </div>
+        )  : (
+          <>
+            <Link to="/new" className="new">
+              <FiPlus size={25} color="#FFF" />
+              Novo chamado
+            </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                {chamados.map((item, index)=>{
+                  return(
+                    <tr key={index}>
+                      <td data-label="Cliente">{item.cliente}</td>
+                      <td data-label="Assunto">{item.assunto}</td>
+                      <td data-label="Status">
+                        <span className="badge" style={{ backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999' }}>{item.status}</span>
+                      </td>
+                      <td data-label="Cadastrado">{item.createdFormated}</td>
+                      <td data-label="#">
+                        <button className="action" style={{backgroundColor: '#3583f6' }} onClick={ () => togglePostModal(item) }>
+                          <FiSearch color="#FFF" size={17} />
+                        </button>
+                        <Link className="action" style={{backgroundColor: '#F6a935' }} to={`/new/${item.id}`} >
+                          <FiEdit2 color="#FFF" size={17} />
+                        </Link>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+            
+
+          </>
+        )}
+
       </div>
+
+      {showPostModal && (
+        <Modal
+          conteudo={detail}
+          close={togglePostModal}
+        />
+      )}
+
     </div>
   )
 }
